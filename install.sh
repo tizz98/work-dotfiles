2acbf01605bbb4d7a389384f8d9f1d7884c12cb2
Eli Wilson <eli@runwayml.com>
2024-01-30 17:48:47 -0500

    Use a brewfile

--- install.sh
+++ install.sh
@@ -8,53 +8,14 @@ then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-if ! command -v chezmoi &> /dev/null
-then
-    echo "Installing chezmoi"
-    brew install chezmoi
-fi
+brew bundle install --file=Brewfile
 
 echo "Installing dotfiles with chezmoi"
 chezmoi init --apply tizz98/work-dotfiles
-
-if ! command -v fzf &> /dev/null
-then
-    echo "Installing fzf"
-    brew install fzf
-    $(brew --prefix)/opt/fzf/install
-fi
-
-if ! command -v starship &> /dev/null
-then
-    echo "Installing starship terminal"
-    brew install starship
-fi
 
 # If linux, install packages with apt
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "Installing packages with apt"
     apt update && apt install -y \
-	build-essential \
-        curl \
-        wget \
-        git \
-        htop \
-        jq \
-        ripgrep \
-        vim
-fi
-
-if command -v code &> /dev/null; then
-    EXTENSIONS=(
-        "ms-python.python"
-        "github.copilot"
-        "charliermarsh.ruff"
-        "ms-kubernetes-tools.vscode-kubernetes-tools"
-        "hashicorp.terraform"
-    )
-
-    echo "Installing ${#EXTENSIONS[@]} vscode extensions"
-    for extension in "${EXTENSIONS[@]}"; do
-        code --install-extension $extension
-    done
+	    build-essential
 fi
